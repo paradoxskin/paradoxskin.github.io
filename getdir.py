9b821b227cbd7d4fdbc4a040b7664501bdcb2a9b
paradoxskin <1312269430@qq.com>
2022-09-08 19:53:25 +0800

    test the init

--- getdir.py
+++ getdir.py
@@ -1,5 +1,11 @@
 import os
-file=open('info','w')
-for i in os.listdir():
-	file.write(i+'\n')
-file.close()
+"""
+ * 游戏命名: gamename_cartpath.html
+ * 博客命名: title_maincontent_tag_time
+"""
+#file=open('post','w')
+file2=open('game','w')
+for i in os.listdir("games"):
+	if("html" in i):
+		file2.write(i+'\n')
+file2.close()
--- js/script.js
+++ js/script.js
@@ -2,3 +2,11 @@ function p8clickTag(data){
 	let cart=document.getElementById("cart");
 	cart.src=data.id
 }
+function checkgame(){
+	var tmp=jQuery.get("https://paradoxskin.github.io/game").responseText.split('\n');
+	for(var i=0;i<tmp.length();i++){
+		var tmpls=i.split("_")
+		gamesName[i]=tmpls[0];
+		gamesCart[i]=tmpls[1].slice(0,tmpls[1].length-5);
+	}
+}
