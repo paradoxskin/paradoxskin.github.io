d5e556407293ffd34591f63ff8ae6a81190864f1
paradoxskin <1312269430@qq.com>
2022-09-09 23:55:47 +0800

    add new function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,26 @@
-function p8clickTag(data){
+function p8clickTag(event){
 	let cart=document.getElementById("cart");
-	cart.src=data.id
+	let gamelink=document.querySelector(".gamelink");
+	cart.src="//paradoxskin.github.io/carts/"+gamesCart[this.id];
+	gamelink.href="//paradoxskin.github.io/games"+gamesName[this.id]+gamesCart[this.id]+".html";
 }
 function checkgame(){
 	var xmlhttp=new XMLHttpRequest();
 	xmlhttp.onreadystatechange=function(){
 		if(xmlhttp.readyState==4 && xmlhttp.status==200){
 			var tmpt=xmlhttp.responseText.split("\n");
+			const mn=document.querySelector(".menu");
 			for(var i=0;i<tmpt.length-1;i++){ // last line is empty
 				var tmpls=tmpt[i].split("_");
 				gamesName[i]=tmpls[0];
 				gamesCart[i]=tmpls[1].slice(0,tmpls[1].length-5);
+				var tmpli=document.createElement("li");
+				var tmpspan=document.createElement("span");
+				tmpspan.id=i;
+				tmpspan.textContent=gamesName[i];
+				tmpspan.addEventListener('click',p8clickTag);
+				tmpli.appendChild(tmpspan);
+				mn.appendChild(tmpli);
 			}
 		}
 	}
